3df18c978dce8d6a0e286621b47b60b847b33086
hiroki-harada <poketgame.0604@gmail.com>
2022-10-10 04:51:23 +0000

    [mod] compile error already resolved question at Beginners Selection _1 / [del] mis-arranged file

--- SpecialContest/BeginnersSelection/_1/Coins.java
+++ SpecialContest/BeginnersSelection/_1/Coins.java
@@ -1,20 +1,20 @@
-import java.util.*;
+// import java.util.*;
 
 class Coins {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        int x = sc.nextInt();
-        sc.close();
+        // Scanner sc = new Scanner(System.in);
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int c = sc.nextInt();
+        // int x = sc.nextInt();
+        // sc.close();
 
-        int ans = 0;
+        // int ans = 0;
 
-        if (x >= 500) {
-            x = 
-        }
+        // if (x >= 500) {
+        //     x =
+        // }
 
 
     }
