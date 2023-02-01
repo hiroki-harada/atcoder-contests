baf27dbfadc82e78947a5d546383d636d574b5f1
hiroki-harada <poketgame.0604@gmail.com>
2023-02-01 13:29:20 +0000

    [tmp] C_1 ABC267

--- BeginnerContest/267/_1/IndexAContinuous.java
+++ BeginnerContest/267/_1/IndexAContinuous.java
@@ -4,9 +4,25 @@ public class IndexAContinuous {
     public static void main(String[] args) {
 
         IOHandler io = new IOHandler();
-
+        int n = io.nextInt();
+        int m = io.nextInt();
+        int[] a = io.nextIntArray(n);
         io.close();
 
+        // sum[i]: 0 から i-1 までの値について、 1*B0 + ... + (i-1)*Bi-1 の値
+        // 1 <= i <= n
+        // https://knowledge.sakura.ad.jp/24150/
+        int[] sum = new int[n];
+        sum[0] = a[0];
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                sum[i] += a[j];
+            }
+        }
+
+        long result = 0;
+
+        io.output(result);
     }
 
     private static class IOHandler {
@@ -26,6 +42,7 @@ public class IndexAContinuous {
         }
         private void output(int result) {System.out.println(result);}
         private <T> void output(T result) {System.out.println(result);}
+        private void output(long result) {System.out.println(result);}
         private void outputIntArray(int[] array, String delimiter) {
             StringBuilder result = new StringBuilder();
             for (int i = 0; i < array.length; i++)  result.append(array[i]).append(delimiter);
