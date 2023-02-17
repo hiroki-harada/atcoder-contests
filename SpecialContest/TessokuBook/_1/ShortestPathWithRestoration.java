559ddac49b43ca8c7f8a4924e8d5b0b4579c6dce
hiroki-harada <poketgame.0604@gmail.com>
2023-02-17 14:58:33 +0000

    [tmp] improve output in B64 TessokuBook

--- SpecialContest/TessokuBook/_1/ShortestPathWithRestoration.java
+++ SpecialContest/TessokuBook/_1/ShortestPathWithRestoration.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.stream.*;
 
 public class ShortestPathWithRestoration {
     public static void main(String[] args) {
@@ -7,10 +8,10 @@ public class ShortestPathWithRestoration {
         int n = io.nextInt();
         int m = io.nextInt();
         List<ArrayList<Edge>> graph = new ArrayList<>();
-        for (int i = 0; i <= n; i++) graph.add(new ArrayList<>());
+        for (int i = 0; i < n; i++) graph.add(new ArrayList<>());
         for (int i = 0; i < m; i++) {
-            int a = io.nextInt();
-            int b = io.nextInt();
+            int a = io.nextInt()-1;
+            int b = io.nextInt()-1;
             int c = io.nextInt();
             graph.get(a).add(new Edge(b, c));
             graph.get(b).add(new Edge(a, c));
@@ -44,11 +45,10 @@ public class ShortestPathWithRestoration {
 
         // 経路の復元
         List<Integer> result = new ArrayList<>();
-        int current = n;
-        result.add(current);
+        int current = n-1;
         while (true) {
             result.add(current);
-            if (current == 1) break;
+            if (current == 0) break;
 
             for (int i = 0, size = graph.get(current).size(); i < size; i++) {
                 int previous = graph.get(current).get(i).to;
@@ -59,32 +59,16 @@ public class ShortestPathWithRestoration {
                 }
             }
         }
-        result.sort((i, j) -> j - i);
-        io.outputArray(result.toArray(new Integer[result.size()]), null);
+
+        // 0 origin -> 1 origin and sort
+        result.stream().map(i -> ++i).sorted((i, j) -> i - j).collect(Collectors.toList());
+        io.outputArray(result.toArray(new Integer[result.size()]), " ");
     }
 
     private static class IOHandler {
         private Scanner sc = new Scanner(System.in);
         private void close() {this.sc.close();}
         private int nextInt() {return this.sc.nextInt();}
-        private String nextStr() {return this.sc.next();}
-        private int[] nextIntArray(int size) {
-            int[] array = new int[size];
-            for (int i = 0; i < size; i++) array[i] = this.sc.nextInt();
-            return array;
-        }
-        private String[] nextStrArray(int size) {
-            List<String> list = new ArrayList<>();
-            for (int i = 0; i < size; i++) list.add(this.sc.next());
-            return list.toArray(new String[size]);
-        }
-        private void output(int result) {System.out.println(result);}
-        private <T> void output(T result) {System.out.println(result);}
-        private void outputIntArray(int[] array, String delimiter) {
-            StringBuilder result = new StringBuilder();
-            for (int i = 0; i < array.length; i++)  result.append(array[i]).append(delimiter);
-            System.out.println(result.toString().substring(0, result.length()-delimiter.length()));
-        }
         private <T> void outputArray(T[] array, String delimiter) {
             StringBuilder result = new StringBuilder();
             for (int i = 0; i < array.length; i++)  result.append(array[i]).append(delimiter);
