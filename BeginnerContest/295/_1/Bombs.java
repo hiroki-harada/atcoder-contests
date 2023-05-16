7b0e710baf3c6a14b9bce3bbc8de7dd7c8a315e8
hiroki-harada <poketgame.0604@gmail.com>
2023-05-16 14:57:02 +0000

    [tmp] B_1 ABC295

---
+++ BeginnerContest/295/_1/Bombs.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+public class Bombs {
+    public static void main(String[] args) {
+
+        IOHandler io = new IOHandler();
+        int r = io.nextInt();
+        int c = io.nextInt();
+        String[] b = io.nextStrArray(r);
+        io.close();
+
+        String[] result = new String[r];
+        for (int i = 0; i < r; i++) {
+            String bi = b[i];
+
+            for (String bij : bi.split("")) {
+                if (bij.matches("[0-9]")) {
+                    for (int k = 0; k < result.length; k++) {
+
+                    }
+                }
+            }
+            result[i] = bi;
+        }
+
+        io.outputArray(result, "\n");
+    }
+
+    private static class IOHandler {
+        private Scanner sc = new Scanner(System.in);
+        private void close() {this.sc.close();}
+        private int nextInt() {return this.sc.nextInt();}
+        private long nextLong() {return this.sc.nextLong();}
+        private String nextStr() {return this.sc.next();}
+        private int[] nextIntArray(int size) {
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++) array[i] = this.sc.nextInt();
+            return array;
+        }
+        private int[][] nextIntArray(int size1, int size2) {
+           int[][] array = new int[size1][size2];
+           for (int i = 0; i < size1; i++) array[i] = nextIntArray(size2);
+           return array;
+        }
+        private long[] nextLongArray(int size) {
+            long[] array = new long[size];
+            for (int i = 0; i < size; i++) array[i] = this.sc.nextLong();
+            return array;
+        }
+        private String[] nextStrArray(int size) {
+            List<String> list = new ArrayList<>();
+            for (int i = 0; i < size; i++) list.add(this.sc.next());
+            return list.toArray(new String[size]);
+        }
+        private void output(int result) {System.out.println(result);}
+        private <T> void output(T result) {System.out.println(result);}
+        private void outputIntArray(int[] array, String delimiter) {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < array.length; i++)  result.append(array[i]).append(delimiter);
+            System.out.println(result.toString().substring(0, result.length()-delimiter.length()));
+        }
+        private <T> void outputArray(T[] array, String delimiter) {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < array.length; i++)  result.append(array[i]).append(delimiter);
+            System.out.println(result.toString().substring(0, result.length()-delimiter.length()));
+        }
+        private <T> void outputList(List<T> list, String delimiter) {
+            StringBuilder result = new StringBuilder();
+            for (T e : list)  result.append(e).append(delimiter);
+            System.out.println(result.toString().substring(0, result.length()-delimiter.length()));
+        }
+    }
+}
