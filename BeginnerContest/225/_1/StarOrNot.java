508a2aa68e7aba75e5e3442fd1f5e2ed4c44286f
hiroki-harada <poketgame.0604@gmail.com>
2023-06-08 15:12:39 +0000

    [add] B_1 ABC225

--- BeginnerContest/225/_1/StarOrNot.java
+++ BeginnerContest/225/_1/StarOrNot.java
@@ -4,53 +4,31 @@ public class StarOrNot {
     public static void main(String[] args) {
 
         IOHandler io = new IOHandler();
-
+        var n = io.nextInt();
+        var graph = new ArrayList<List<Integer>>();
+        for (int i = 0; i < n; i++) graph.add(new ArrayList<>());
+        for (int i = 0; i < n-1; i++) {
+            int a = io.nextInt()-1;
+            int b = io.nextInt()-1;
+            graph.get(a).add(b);
+            graph.get(b).add(a);
+        }
         io.close();
 
+        for (int i = 0; i < n; i++) {
+            int size = graph.get(i).size();
+            if (size == n-1) {
+                io.output("Yes");
+                return;
+            }
+        }
+        io.output("No");
     }
 
     private static class IOHandler {
         private Scanner sc = new Scanner(System.in);
         private void close() {this.sc.close();}
         private int nextInt() {return this.sc.nextInt();}
-        private long nextLong() {return this.sc.nextLong();}
-        private String nextStr() {return this.sc.next();}
-        private int[] nextIntArray(int size) {
-            int[] array = new int[size];
-            for (int i = 0; i < size; i++) array[i] = this.sc.nextInt();
-            return array;
-        }
-        private int[][] nextIntArray(int size1, int size2) {
-           int[][] array = new int[size1][size2];
-           for (int i = 0; i < size1; i++) array[i] = nextIntArray(size2);
-           return array;
-        }
-        private long[] nextLongArray(int size) {
-            long[] array = new long[size];
-            for (int i = 0; i < size; i++) array[i] = this.sc.nextLong();
-            return array;
-        }
-        private String[] nextStrArray(int size) {
-            List<String> list = new ArrayList<>();
-            for (int i = 0; i < size; i++) list.add(this.sc.next());
-            return list.toArray(new String[size]);
-        }
-        private void output(int result) {System.out.println(result);}
         private <T> void output(T result) {System.out.println(result);}
-        private void outputIntArray(int[] array, String delimiter) {
-            StringBuilder result = new StringBuilder();
-            for (int i = 0; i < array.length; i++)  result.append(array[i]).append(delimiter);
-            System.out.println(result.toString().substring(0, result.length()-delimiter.length()));
-        }
-        private <T> void outputArray(T[] array, String delimiter) {
-            StringBuilder result = new StringBuilder();
-            for (int i = 0; i < array.length; i++)  result.append(array[i]).append(delimiter);
-            System.out.println(result.toString().substring(0, result.length()-delimiter.length()));
-        }
-        private <T> void outputList(List<T> list, String delimiter) {
-            StringBuilder result = new StringBuilder();
-            for (T e : list)  result.append(e).append(delimiter);
-            System.out.println(result.toString().substring(0, result.length()-delimiter.length()));
-        }
     }
 }
