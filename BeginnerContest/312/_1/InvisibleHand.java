49ca549c0bcf88bd1794dee926f06c61be2d367e
hiroki-harada <poketgame.0604@gmail.com>
2023-08-04 14:59:54 +0000

    [tmp] C_1 ABC312

---
+++ BeginnerContest/312/_1/InvisibleHand.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class InvisibleHand {
+    public static void main(String[] args) {
+
+        IOHandler io = new IOHandler();
+        int n = io.nextInt();
+        int m = io.nextInt();
+
+        int[] a = io.nextIntArray(n);
+        int[] b = io.nextIntArray(m);
+        io.close();
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+    }
+
+    private static class IOHandler {
+        private Scanner sc = new Scanner(System.in);
+        private void close() {this.sc.close();}
+        private int nextInt() {return this.sc.nextInt();}
+        private int[] nextIntArray(int size) {
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++) array[i] = this.sc.nextInt();
+            return array;
+        }
+        private void output(int result) {System.out.println(result);}
+        private <T> void output(T result) {System.out.println(result);}
+        private void outputIntArray(int[] array, String delimiter) {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < array.length; i++)  result.append(array[i]).append(delimiter);
+            System.out.println(result.toString().substring(0, result.length()-delimiter.length()));
+        }
+        private <T> void outputArray(T[] array, String delimiter) {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < array.length; i++)  result.append(array[i]).append(delimiter);
+            System.out.println(result.toString().substring(0, result.length()-delimiter.length()));
+        }
+        private <T> void outputList(List<T> list, String delimiter) {
+            StringBuilder result = new StringBuilder();
+            for (T e : list)  result.append(e).append(delimiter);
+            System.out.println(result.toString().substring(0, result.length()-delimiter.length()));
+        }
+    }
+}
