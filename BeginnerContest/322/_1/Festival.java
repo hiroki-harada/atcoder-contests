61cefcccaed2c3fd164ecfc91039931d7da93dd0
hiroki-harada <poketgame.0604@gmail.com>
2023-09-30 14:12:05 +0000

    [mod] improve InputHandler

--- BeginnerContest/322/_1/Festival.java
+++ BeginnerContest/322/_1/Festival.java
@@ -1,19 +1,13 @@
-import java.io.*;
 import java.util.*;
 
 public class Festival {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-
-        st = new StringTokenizer(br.readLine());
-        int[] a = new int[m];
-        for (int i = 0; i < m; i++) {
-            a[i] = Integer.parseInt(st.nextToken());
-        }
+        InputHandler in = new InputHandler();
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int[] a = in.nextIntArray(m);
+        in.close();
 
         // 花火が上がる日：1, 花火が上がらない日：0
         int[] aryFireWorksDays = new int[n];
@@ -33,5 +27,16 @@ public class Festival {
         }
 
         System.out.println(String.join("\n", result));
+    }
+
+    private static class InputHandler {
+        private Scanner sc = new Scanner(System.in);
+        private void close() {this.sc.close();}
+        private int nextInt() {return this.sc.nextInt();}
+        private int[] nextIntArray(int size) {
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++) array[i] = this.sc.nextInt();
+            return array;
+        }
     }
 }
