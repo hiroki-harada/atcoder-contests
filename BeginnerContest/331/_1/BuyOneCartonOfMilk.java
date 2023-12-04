019a7500c7eea2ab2db2f97263d6c8ea20686148
hiroki-harada <poketgame.0604@gmail.com>
2023-12-04 14:21:53 +0000

    [add] complete B_1 ABC331

---
+++ BeginnerContest/331/_1/BuyOneCartonOfMilk.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class BuyOneCartonOfMilk {
+    public static void main(String[] args) {
+
+        InputHandler in = new InputHandler();
+        int n = in.nextInt();
+        int s = in.nextInt();
+        int m = in.nextInt();
+        int l = in.nextInt();
+        in.close();
+
+        // 購入個数パック数が100を超える最初のindexまで、適当な回数ループさせる
+        int result = Integer.MAX_VALUE;
+        for (int i = 0; i*6 <= 102; i++) {
+            for (int j = 0; j*8 <= 104; j++) {
+                for (int k = 0; k*12 <= 108; k++) {
+                    if (i*6 + j*8 + k*12 < n) continue;
+                    result = Math.min(result, s*i + m*j + l*k);
+                }
+            }
+        }
+
+        System.out.println(result);
+    }
+
+    private static class InputHandler {
+        private Scanner sc = new Scanner(System.in);
+        private void close() {this.sc.close();}
+        private int nextInt() {return this.sc.nextInt();}
+    }
+}
