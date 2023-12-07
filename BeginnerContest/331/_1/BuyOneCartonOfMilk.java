167135e5146ca943e239281cf3788fdd5643105c
hiroki-harada <poketgame.0604@gmail.com>
2023-12-07 13:26:48 +0000

    [mod] generalize count of roops at B in ABC331

--- BeginnerContest/331/_1/BuyOneCartonOfMilk.java
+++ BeginnerContest/331/_1/BuyOneCartonOfMilk.java
@@ -12,9 +12,9 @@ public class BuyOneCartonOfMilk {
 
         // 購入個数パック数が100を超える最初のindexまで、適当な回数ループさせる
         int result = Integer.MAX_VALUE;
-        for (int i = 0; i*6 <= 102; i++) {
-            for (int j = 0; j*8 <= 104; j++) {
-                for (int k = 0; k*12 <= 108; k++) {
+        for (int i = 0; i*6 < 100+6; i++) {
+            for (int j = 0; j*8 < 100+8; j++) {
+                for (int k = 0; k*12 < 100+12; k++) {
                     if (i*6 + j*8 + k*12 < n) continue;
                     result = Math.min(result, s*i + m*j + l*k);
                 }
