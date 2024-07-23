a8efcdcc2bdd16d3bbdd06667d538bce6778ecbf
hiroki-harada <poketgame.0604@gmail.com>
2024-07-23 15:09:10 +0000

    [add] complete 020 with kotlin

--- SpecialContest/Typical90/_1/LogInequality.kt
+++ SpecialContest/Typical90/_1/LogInequality.kt
@@ -1,12 +1,10 @@
-import kotlin.math.*
 import java.math.BigDecimal
 
 fun main() {
-    val (a, b, c) = readln().split(" ").map { it.toDouble() }
+    val input = readln().split(" ")
+    val a = input[0].toBigDecimal()
+    val b = input[1].toInt()
+    val c = input[2].toBigDecimal()
 
-    println(BigDecimal(log(a, 2.0)))
-    println(BigDecimal(b * log(c, 2.0)))
-    println(a)
-    println(c.pow(b))
     println(if (a < c.pow(b)) "Yes" else "No")
 }
