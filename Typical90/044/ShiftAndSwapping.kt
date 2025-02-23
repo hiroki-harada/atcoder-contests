82941f580b1eddc499d4bfdadf725086cea7f8b0
hiroki-harada <poketgame.0604@gmail.com>
2025-02-23 23:58:47 +0000

    tmp

---
+++ Typical90/044/ShiftAndSwapping.kt
@@ -0,0 +1,19 @@
+fun main() {
+    val (n, q) = readln().split(" ").map { it.toInt() }
+    val a = readln().split(" ").map { it.toInt() }
+
+
+    for (i in 0 until q) {
+        val (t, x, y) = readln().split(" ").map { it.toInt() }
+
+        if (t == 1) {
+
+        }
+        if (t == 2) {
+
+        }
+        if (t == 3) {
+
+        }
+    }
+}
